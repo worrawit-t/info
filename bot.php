249575c8bebdcf8af4b90d1f5ca6cb3090c9d6b4
worrawit-t <56953509+worrawit-t@users.noreply.github.com>
2019-10-24 15:31:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,13 +9,13 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
-	console_log("!is_null($events[ESP])");
+	echo("!is_null($events[ESP])");
 	send_LINE($events['ESP']);
 		
 	echo "OK";
 	}
 if (!is_null($events['events'])) {
-	console_log("!is_null($events[events])");
+	echo("!is_null($events[events])");
 	echo "line bot";
 	// Loop through each event
 	foreach ($events['events'] as $event) {
