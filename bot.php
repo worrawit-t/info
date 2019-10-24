38479f878b2caeb9a577cf6ccb9d0966d0213548
worrawit-t <56953509+worrawit-t@users.noreply.github.com>
2019-10-24 15:23:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
  require("pub.php");
  require("line.php");
-
+console_log("--------------------------------------------------");
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
